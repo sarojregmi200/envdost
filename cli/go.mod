0b5542633828174b378625204a66a9acb8713b06
saroj regmi <sarojregmi.official@gmail.com>
2023-06-30 14:28:49 +0545

    Update: environment variable terminal longterm session complete

--- cli/go.mod
+++ cli/go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 )
--- cli/main.go
+++ cli/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
-	"envdost/cmd"
 	_ "envdost/cmd/delete"
+	"fmt"
+
+	"golang.org/x/sys/windows/registry"
 )
 
 func main() {
-	cmd.Execute()
+	// cmd.Execute()
+
+
+
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, `SYSTEM\CurrentControlSet\Control\Session Manager\Environment`, registry.ALL_ACCESS)
+	if err != nil {
+		fmt.Println("Failed to open registry key:", err)
+		return
+	}
+	defer key.Close()
+
+	// // Set the environment variable
+	// err = key.SetStringValue("MY_VARIABLE", "my_value")
+	// if err != nil {
+	// 	fmt.Println("Failed to set environment variable:", err)
+	// 	return
+	// }
+
+	out, _,err := key.GetStringValue("MY_VARIABLE")
+	
+	fmt.Println(out)
+
+
+	 
 }
